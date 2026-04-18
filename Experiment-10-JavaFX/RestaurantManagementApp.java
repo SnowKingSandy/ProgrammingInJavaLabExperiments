cbe8133fa0cc245f97325c602a4a8e7e410bed03
SnowKingSandy <sarveshdungarwal@gmail.com>
2026-04-18 14:07:26 +0530

    fix: Add missing GridPane import to RestaurantManagementApp for JavaFX compilation

--- Experiment-10-JavaFX/RestaurantManagementApp.java
+++ Experiment-10-JavaFX/RestaurantManagementApp.java
@@ -3,6 +3,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
